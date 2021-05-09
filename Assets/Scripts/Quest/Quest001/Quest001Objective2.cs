056793e9b7ded6f729b4989d75e4b657e19f3388
junaid-ali-butt <70067695@student.uol.edu.pk>
2021-05-09 22:40:40 +0500

    Treasure Chest Animation and Sword Added

---
+++ Assets/Scripts/Quest/Quest001/Quest001Objective2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest001Objective2 : MonoBehaviour
+{
+    public float TheDistance;
+    public GameObject TreasureChest;
+    public GameObject ActionText;
+    public GameObject ActionDisplay;
+    public GameObject TheObjective;
+    public int CloseObjective;
+    public GameObject TakeSword;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+
+        if(CloseObjective == 3){
+            if(TheObjective.transform.localScale.y <= 0.0f){
+                CloseObjective=0;
+                TheObjective.SetActive (false);
+            }else{
+                TheObjective.transform.localScale -= new Vector3 (0.0f, 0.1f, 0.0f);
+            }
+        }
+    }
+
+    void OnMouseOver() {
+        if(TheDistance <=2){
+            ActionDisplay.GetComponent<Text>().text="Open Chest";
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+
+        if(Input.GetButtonDown("Action")){
+            if(TheDistance<=2){
+                this.GetComponent<BoxCollider> ().enabled = false;
+				TreasureChest.GetComponent<Animation> ().Play ("OpenChestAnim");
+                // TheObjective.SetActive(true);
+				TakeSword.SetActive (true);
+				CloseObjective = 3;
+				ActionText.SetActive (false);
+				ActionDisplay.SetActive (false);
+            }
+        }
+    }
+
+    void OnMouseExit () {
+		ActionDisplay.SetActive (false);
+		ActionText.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Quest/Quest001/Quest001Objective3.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest001Objective3 : MonoBehaviour
+{
+    public float TheDistance;
+	public GameObject FakeSword;
+	public GameObject RealSword;
+	public GameObject ActionText;
+	public GameObject ActionDisplay;
+	public GameObject TheObjective;
+	public int CloseObjective;
+	public GameObject ChestBlock;
+
+	void Update () {
+		TheDistance = PlayerCasting.DistanceFromTarget;
+
+		if (CloseObjective == 3) {
+			if (TheObjective.transform.localScale.y <= 0.0f) {
+				CloseObjective = 0;
+				TheObjective.SetActive (false);
+			} else {
+				TheObjective.transform.localScale -= new Vector3 (0.0f, 0.01f, 0.0f);
+			}
+		}
+		
+	}
+
+	void OnMouseOver () {
+		if (TheDistance <= 3) {
+			ActionDisplay.GetComponent<Text> ().text = "Take Sword";
+			ActionDisplay.SetActive (true);
+			ActionText.SetActive (true);
+		}
+
+		if (Input.GetButtonDown ("Action")) {
+			if (TheDistance <= 3) {
+				this.GetComponent<BoxCollider> ().enabled = false;
+				FakeSword.SetActive (false);
+				RealSword.SetActive (true);
+				ChestBlock.SetActive (true);
+				CloseObjective = 3;
+				ActionText.SetActive (false);
+				ActionDisplay.SetActive (false);
+			}
+		}
+	}
+
+	void OnMouseExit () {
+		ActionDisplay.SetActive (false);
+		ActionText.SetActive (false);
+	}
+}
