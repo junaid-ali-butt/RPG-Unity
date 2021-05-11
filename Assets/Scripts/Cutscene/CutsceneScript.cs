2ba815f1bcbdc5d41a5b6573320df25e4d5fb0d0
junaid-ali-butt <70067695@student.uol.edu.pk>
2021-05-12 00:06:56 +0500

    Start Cutscene and Enemy Added

---
+++ Assets/Scripts/Cutscene/CutsceneScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneScript : MonoBehaviour
+{
+    public GameObject Camera01;
+    public GameObject Camera02;
+    public GameObject Camera03;
+    public GameObject Player;
+    public GameObject FadeIn;
+    public GameObject FadeOut;
+    public GameObject CutsceneText1;
+    public GameObject CutsceneText2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (CutSceneStart());
+    }
+
+    IEnumerator CutSceneStart(){
+        yield return new WaitForSeconds(5);
+        Camera02.SetActive(true);
+        CutsceneText1.SetActive(false);
+        CutsceneText2.SetActive(false);
+        Camera01.SetActive(false);
+        FadeIn.SetActive(false);
+        yield return new WaitForSeconds(10);
+        Camera03.SetActive(true);
+        Camera02.SetActive(false);
+        yield return new WaitForSeconds(4);
+        FadeOut.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Player.SetActive(true);
+        FadeIn.SetActive(true);
+        FadeOut.SetActive(false);
+        Camera03.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/SpiderEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderEnemy : MonoBehaviour
+{
+    public int EnemyHealth = 10;
+
+	void DeductPoints (int DamageAmount) {
+		EnemyHealth -= DamageAmount;
+	}
+    
+	void Update () {
+		if (EnemyHealth <= 0) {
+			StartCoroutine (DeathSpider ());
+		}
+	}
+	IEnumerator DeathSpider () {
+		yield return new WaitForSeconds (0.5f);
+		Destroy (gameObject);
+	}
+}
