ea8ee7006f8f905768e7d9891f162647947c6726
junaid-ali-butt <70067695@student.uol.edu.pk>
2021-05-13 00:53:05 +0500

    NPC, Dialogue & Enemy AI Added

---
+++ Assets/Scripts/NPCInteractions/NPC001.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC001 : MonoBehaviour
+{
+    public float TheDistance;
+	public GameObject ActionDisplay;
+	public GameObject ActionText;
+	public GameObject ThePlayer;
+    public GameObject TextBox;
+    public GameObject NPCName;
+    public GameObject NPCText;
+
+	void Update () {
+		TheDistance = PlayerCasting.DistanceFromTarget;
+	}
+
+	void OnMouseOver () {
+		if (TheDistance <= 3) {
+			SwordBlocker.SwordBlock=1;
+            ActionDisplay.GetComponent<Text>().text="Talk";
+			ActionDisplay.SetActive (true);
+			ActionText.SetActive (true);
+		}
+
+		if (Input.GetButtonDown ("Action")) {
+			if (TheDistance <= 3) {
+                SwordBlocker.SwordBlock=2;
+				ActionDisplay.SetActive (false);
+				ActionText.SetActive (false);
+				//ThePlayer.SetActive (false);
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+                StartCoroutine (NPC001Active());
+			}
+		}
+	}
+
+	void OnMouseExit() {
+		SwordBlocker.SwordBlock=0;
+		ActionDisplay.SetActive (false);
+		ActionText.SetActive (false);
+	}
+
+    IEnumerator NPC001Active(){
+        TextBox.SetActive(true);
+        NPCName.GetComponent<Text>().text="Tim";
+        NPCName.SetActive(true);
+        NPCText.GetComponent<Text>().text="Hello there! I have a quest for you! Should you accept it, please come back here later";
+        NPCText.SetActive(true);
+        yield return new WaitForSeconds(5.5f);
+        NPCName.SetActive(false);
+        NPCText.SetActive(false);
+        TextBox.SetActive(false);
+        ActionDisplay.SetActive (true);
+		ActionText.SetActive (true);
+    }
+}
